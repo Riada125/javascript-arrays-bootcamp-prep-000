e0a3ed457906802a24ede9594a5309db9ab247b8
Michael Adair <riada125@hotmail.com>
2019-07-28 22:22:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -43,4 +43,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array;
 }
 
-function 
+function removeElementFromEndOfArray(array){
+  array.slice(0, array.length - 1);
+  return array;
+}
